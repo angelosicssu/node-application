89a52eb6d7f07aba51153f692c9850d6db319b09
Ângelo Gabriel <angelogabrielsicssu@gmail.com>
2025-02-06 14:15:05 -0400

    feat: fs module async

--- fs-module-async.js
+++ fs-module-async.js
@@ -1,7 +1,25 @@
-const { readFileSync, writeFileSync } = require('fs')
-
-const first = readFileSync('./content/first.txt', 'utf-8')
-const second = readFileSync('./content/second.txt', 'utf-8')
-console.log(first, second)
-
-writeFileSync('./content/result-sync.txt', 'Hello World')
+const { readFile, writeFile } = require('fs');
+readFile('./content/first.txt', 'utf-8', (err, result) => {
+  if(err) {
+    console.log(err);
+    return
+  }
+  const first = result
+  readFile('./content/first.txt', 'utf-8', (err, result) => {
+    if(err) {
+      console.log(err);
+      return
+    }
+    const second = result;
+    writeFile('./content/result-async.txt',
+      `Here is the result: ${first} and ${second}`,
+      (err, result) => {
+        if(err) {
+          console.log(err);
+          return
+        } 
+        console.log(result)
+      }
+    )
+  })
+})
